c99356a77637325c8ffc7158b8f81823b7050dfd
Luca Carlon <carlon.luca@gmail.com>
2025-11-22 12:56:51 +0100

    Added license header.

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,23 @@
+/*
+ * Project: Concierge
+ * Date:    2025.11.22
+ *
+ * Copyright (C) 2025 Luca Carlon
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <Adafruit_TinyUSB.h>
 #include <bluefruit.h>
 
@@ -17,5 +37,4 @@ void setup() {
   welcomeBeacon.startAdvertising();
 }
 
-void loop() {
-}
+void loop() {}
